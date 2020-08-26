8500af577a4076ee8a6dd56e81f3b29edfd93702
WPR1102 <1158463714@qq.com>
2020-08-26 16:03:38 +0800

    增加了一个SysDemoController,用来做swagger接口，里面有查询楼栋楼层会议室三级数据,增加了增删改查

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysDemoController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysDemoController.java
@@ -2,13 +2,14 @@ package com.ruoyi.web.controller.system;
 
 import com.ruoyi.common.core.domain.AjaxResult;
 import com.ruoyi.system.domain.SysBuilding;
+import com.ruoyi.system.domain.SysConfOrder;
 import com.ruoyi.system.service.ISysBuildingService;
+import com.ruoyi.system.service.ISysConfOrderService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.ruoyi.common.core.controller.BaseController;
 import java.util.List;
 
@@ -19,15 +20,63 @@ public class SysDemoController extends BaseController {
     @Autowired
     private ISysBuildingService sysBuildingService;
 
+    @Autowired
+    private ISysConfOrderService sysConfOrderService;
+
     /**
      * 查询楼栋信息列表
      */
     @ApiOperation("获取三级列表信息")
-    @GetMapping("/test/demo1/list")
-    public AjaxResult list()
-    {
+    @GetMapping("/listThreeConf")
+    public AjaxResult listThree() {
         List<SysBuilding> list = sysBuildingService.selectSysBuildingAllDate(null);
         return AjaxResult.success(list);
     }
 
+    /**
+     * 查询会议室预约列表
+     */
+    @ApiOperation("获取会议室预约信息列表")
+    @GetMapping("/listConfOrder")
+    public AjaxResult listConfOrder() {
+        List<SysConfOrder> list = sysConfOrderService.selectSysConfOrderList(null);
+        return AjaxResult.success(list);
+    }
+
+    /**
+     * 获取会议室预约详细信息
+     */
+    @ApiOperation("按id获取会议室预约详情")
+    @ApiImplicitParam(name = "confOrderId",value = "会议预约ID",required = true, dataType = "int", paramType = "path")
+    @GetMapping(value = "/{confOrderId}")
+    public AjaxResult getInfo(@PathVariable("confOrderId") Long confOrderId) {
+        return AjaxResult.success(sysConfOrderService.selectSysConfOrderById(confOrderId));
+    }
+
+    /**
+     * 新增
+     */
+    @ApiOperation("新增会议室预约信息")
+    @PostMapping
+    public AjaxResult add(@RequestBody SysConfOrder sysConfOrder) {
+        return toAjax(sysConfOrderService.insertSysConfOrder(sysConfOrder));
+    }
+
+    /**
+     * 修改
+     */
+    @ApiOperation("修改会议室预约信息")
+    @PutMapping
+    public AjaxResult edit(@RequestBody SysConfOrder sysConfOrder) {
+        return toAjax(sysConfOrderService.updateSysConfOrder(sysConfOrder));
+    }
+
+    /**
+     * 删除
+     */
+    @ApiOperation("删除会议室预约信息")
+    @DeleteMapping("/{confOrderIds}")
+    public AjaxResult remove(@PathVariable Long[] confOrderIds) {
+        return toAjax(sysConfOrderService.deleteSysConfOrderByIds(confOrderIds));
+    }
 }
